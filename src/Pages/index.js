c6ee89e6d275b03137827720a3e405f626748ff9
sawa-mon <sawada.6284@gmail.com>
2020-11-17 11:44:01 +0900

    リロード時、ホームディレクトへのリダイレクト実装

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home";
 export { default as GirlsSelect } from "./GirlsSelect";
 export { default as Chat } from "./Chat";
+export { default as StartChat } from "./StartChat";
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Home, GirlsSelect, Chat } from "./Pages/index";
 import { Route, Switch, HashRouter } from "react-router-dom";
+import { StartChat } from "../src/Pages";
 
 const Router = () => {
   return (
     <HashRouter hashType="noslash">
       <Switch>
         <Route exact path={"/"} component={Home} />
-        <Route exact path={"/girls/select"} component={GirlsSelect} />
-        <Route exact path={"/chat"} component={Chat} />
+        <StartChat>
+          <Route exact path={"/girls/select"} component={GirlsSelect} />
+          <Route exact path={"/chat"} component={Chat} />
+        </StartChat>
       </Switch>
     </HashRouter>
   );
